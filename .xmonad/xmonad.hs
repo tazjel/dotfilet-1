9c8c7e00a410c9d1be19ac171704fcf974682f53
Eric Perko <eric@ericp-i7pc.(none)>
2010-03-14 22:43:19 -0400

    Added fullscreen suppoert

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,8 +2,13 @@ import XMonad
 import XMonad.Config.Gnome
 import XMonad.Util.EZConfig
 import XMonad.Hooks.SetWMName
+import XMonad.Hooks.ManageHelpers
  
 main = xmonad $ gnomeConfig
 	{ modMask = mod4Mask
 	, startupHook = startupHook gnomeConfig >> setWMName "LG3D"
+	, manageHook = composeAll 
+	    [ manageHook gnomeConfig
+	    , isFullscreen --> doFullFloat
+	    ]
 	}
