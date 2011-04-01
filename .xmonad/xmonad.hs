2e6054f8a14d9cb5b251c1527bca017d8593a3fa
Eric Perko <wisesage5001@gmail.com>
2011-04-01 03:41:35 -0400

    Now using a GIMP rule so that it doesn't load up tiled and retarded

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,12 +3,17 @@ import XMonad.Config.Gnome
 import XMonad.Util.EZConfig
 import XMonad.Hooks.SetWMName
 import XMonad.Hooks.ManageHelpers
- 
+import qualified XMonad.StackSet as W
+
 main = xmonad $ gnomeConfig
 	{
-	  startupHook = startupHook gnomeConfig >> setWMName "LG3D"
-	, manageHook = composeAll 
-	    [ manageHook gnomeConfig
-	    , isFullscreen --> doFullFloat
-	    ]
+	startupHook = startupHook gnomeConfig >> setWMName "LG3D"
+	, manageHook = myManageHook
 	}
+
+myManageHook = composeAll
+	[ manageHook gnomeConfig
+	, isFullscreen --> doFullFloat
+	, (role =? "gimp-toolbox" <||> role =? "gimp-image-window") --> (ask >>= doF . W.sink)
+	]
+	where role = stringProperty "WM_WINDOW_ROLE"
