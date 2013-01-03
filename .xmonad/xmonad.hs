a7e271b007d3cbe8c5f20fab9a4fe0f7814ef844
Eric Perko <wisesage5001@gmail.com>
2013-01-02 20:03:54 -0500

    Added configuration for xmobar

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,19 +1,35 @@
+import System.IO
+
 import XMonad
 import XMonad.Util.EZConfig
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.SetWMName
+import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Util.Run(spawnPipe)
 import qualified XMonad.StackSet as W
+import qualified Data.Map as M
 
 main = do
 	xmobarproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/xmobar.hs"
 	xmonad $ defaultConfig {
 	  terminal = "/usr/bin/urxvt"
+	  , layoutHook = avoidStruts $ layoutHook defaultConfig
 	  , startupHook = setWMName "LG3D"
-	  , manageHook = myManageHook
+	  , manageHook = myManageHook <+> manageDocks
+	  , keys = \c -> myKeys c `M.union` keys defaultConfig c
+	  , logHook = dynamicLogWithPP $ xmobarPP
+		{
+		ppOutput = hPutStrLn xmobarproc
+		}
 	}
 
-myManageHook = composeAll
+myManageHook = composeAll 
 	[ 
 	  isFullscreen --> doFullFloat
+	]
+
+myKeys (XConfig {modMask = modMask}) = M.fromList $
+	[
+	  ((modMask, xK_b), sendMessage ToggleStruts)
 	]
